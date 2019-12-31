73f65a6445b94196eda9fadaed654609b8087b93
perth <jj61183@gmail.com>
2019-12-31 14:05:26 +0900

    add codec option

--- usb_cam_capture.py
+++ usb_cam_capture.py
@@ -24,26 +24,37 @@ cap = cv2.VideoCapture(0)
 if cap.isOpened() == False:
     print('카메라를 오픈 할 수 없습니다.')
 
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
+#frame_width = int(cap.get(3))
+#frame_height = int(cap.get(4))
+#fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+#out = cv2.VideoWriter('output.avi', fourcc, 25.0, (frame_width,frame_height))
+
+
+frame_width = int(3840)
+frame_height = int(2160)
+
+MJPG_CODEC = 1196444237.0 # MJPG
 
 cv2.namedWindow('Usb Cam', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('Usb Cam', frame_width,frame_height)
 
 bightness = cap.set(cv2.CAP_PROP_BRIGHTNESS, 150)
-print cap.get(cv2.CAP_PROP_BRIGHTNESS)
-
+cap.set(cv2.CAP_PROP_FOURCC, MJPG_CODEC)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 3840)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 2160)
+#print cap.get(cv2.CAP_PROP_BRIGHTNESS)
+print cap.get(cv2.CAP_PROP_FOURCC)
 
 while True:
 	ret, frame = cap.read()
 
 	#change to frame size 
-	frame = cv2.resize(frame, (frame_width, frame_height))
+        #frame = cv2.resize(frame, (frame_width, frame_height))
 	# rotate_frame = imutils.rotate(frame, 0) 
-	# out.write(resize)
 	#frame = Rotate(frame, 90)
 	cv2.imshow('Usb Cam', frame)
-	
+        #out.write(frame)	
+
 	ch = cv2.waitKey(1)
 	if ch == ord('q'):
 	    break
